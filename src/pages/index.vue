c2571c5f4483b0d8277064164519af3897f7217f
adverich <facu.ft@gmail.com>
2022-09-15 04:59:39 -0300

    - mySwitch,  + HomeTitle & HomeSubtitle

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,16 @@
 <template>
-  <v-container>
+  <v-container class="">
     <v-card class="card-border mx-auto blue-lighten-1" width="800">
       <h1>Hello Word</h1>
     </v-card>
+
+    <HomeTitle />
+    <HomeSubtitle />
   </v-container>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'YourComopnentName',
-})
-
-const mySwitch = true
-
-if (mySwitch) {
-  console.log('yes, switch is true')
-}
+  name: "YourComopnentName",
+});
 </script>
