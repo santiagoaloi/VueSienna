c10fd7bdbad2f463df3f5a41371a7dec881d3270
adverich <facu.ft@gmail.com>
2022-09-15 14:06:05 -0300

    + useRouter +button toLogin

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,7 @@
   <v-container class="">
     <v-card class="card-border mx-auto blue-lighten-1" width="800">
       <h1>Hello Word</h1>
+      <v-btn @click="goToLogin" color="#0FA933">Login</v-btn>
     </v-card>
 
     <base-split>
@@ -12,8 +13,12 @@
 </template>
 
 <script setup>
-import BaseSplit from "../components/BaseSplit.vue";
 defineOptions({
   name: "YourComopnentName",
 });
+
+const router = useRouter();
+function goToLogin() {
+  router.push("/public/login");
+}
 </script>
