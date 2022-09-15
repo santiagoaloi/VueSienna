410556f2b7793a237c373d436120029923bf3e18
Santiago Aloi <santiagoaloi@gmail.com>
2022-09-15 19:00:17 +0200

    - transition + eager AppBarLogo

--- src/components/LayouDefault/DefaultAppBarLogo.vue
+++ src/components/LayouDefault/DefaultAppBarLogo.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="d-inline-block">
     <v-img
+      eager
+      :transition="false"
       src="avatar.png"
       height="40"
       width="40"
       class="ml-10 shrink"
-      transition="fab-transition"
     />
   </div>
-  /homebg.svg
 </template>
