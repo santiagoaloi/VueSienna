9d6a171d08c16cf746c00920fa5f8ad33fff3818
Santiago Aloi <santiagoaloi@gmail.com>
2022-09-19 21:25:58 +0200

    Login Screen auth providers buttons

---
+++ src/components/Login/LoginButtons.vue
@@ -0,0 +1,18 @@
+<template>
+  <Base-btn block :key="i" v-for="(button, i) in loginButtons" class="my-3">
+    Signup with {{ button.name }}
+  </Base-btn>
+
+  <div class="d-flex align-center my-4">
+    <v-divider class="grey darken-3" /> <span class="mx-3"> or </span>
+    <v-divider class="grey darken-3" />
+  </div>
+  <Base-btn class="mt-2" block> Continue with email </Base-btn>
+</template>
+
+<script setup>
+const loginButtons = [
+  { name: 'github', icon: '', to: '' },
+  { name: 'google', icon: '', to: '' },
+]
+</script>
