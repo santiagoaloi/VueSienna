e9a864dc37ef6a2eb38ee166142f99b52ef41d09
Santiago Aloi <santiagoaloi@gmail.com>
2022-09-19 21:26:14 +0200

    improved transitions

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,14 +1,12 @@
 <template>
   <v-app>
-    <DefaultAppBar />
-    <v-main>
-      <v-container fluid class="fill-height pa-0 ma-0">
-        <router-view v-slot="{ Component }">
-          <v-fade-transition mode="out-in">
-            <component :is="Component" />
-          </v-fade-transition>
-        </router-view>
-      </v-container>
+    <DAppBar />
+    <v-main scrollable>
+      <router-view v-slot="{ Component }">
+        <v-fade-transition>
+          <component :is="Component" />
+        </v-fade-transition>
+      </router-view>
     </v-main>
   </v-app>
 </template>
