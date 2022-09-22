577f8576609ebf53f9871af15f0e05fa60d2bf46
Santiago Aloi <santiagoaloi@gmail.com>
2022-09-22 06:26:40 +0200

    meta object merge

--- src/utils/autoGenerateRoutes.js
+++ src/utils/autoGenerateRoutes.js
@@ -3,13 +3,13 @@ import { routeModifier } from '../../src/config/route-settings'
 export default routes => {
   const routeConfig = routeModifier()
   routes.forEach(route => {
-    // Injet meta  defined in @/config/route-settings.
+    // Inject meta defined in @/config/route-settings.
     const { component } = route
     for (const rule of routeConfig) {
       const { keyword, meta } = rule
       if (component.includes(keyword)) {
         // Adds component full route path string and meta.
-        route.meta = { ...meta, fullPath: component }
+        route.meta = { ...route.meta, ...meta, fullPath: component }
       }
     }
   })
