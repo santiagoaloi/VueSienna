38eb48b16fb2e93a1882aaa43accc6cfa38b401f
Santiago Aloi <santiagoaloi@gmail.com>
2022-11-25 09:59:29 +0100

    + playground projects

---
+++ src/pages/playground/projects/center-input-text/CenterInputText.vue
@@ -0,0 +1,34 @@
+<template>
+  <VContainer class="d-flex align-center fill-height">
+    <VCard class="mx-auto" width="500 ">
+      <VContainer>
+        <VSwitch
+          v-model="centerInputText"
+          class="mb-n2"
+          color="primary"
+          label="Center input text"
+        />
+        <VTextField
+          variant="outlined"
+          :class="{ 'input-text-center': centerInputText }"
+          class="mb-5"
+        />
+        <VTextField
+          variant="solo"
+          :class="{ 'input-text-center': centerInputText }"
+        />
+      </VContainer>
+    </VCard>
+  </VContainer>
+</template>
+
+<route lang="yaml">
+meta:
+  title: Input text centered
+  description: Attempt of centering the text inside the input component
+  icon: '$mdiFormDropdown'
+</route>
+
+<script setup>
+const centerInputText = ref(false)
+</script>
