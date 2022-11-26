f4f3d409041d4be10f5f697bc86b228b20c9da45
Santiago Aloi <santiagoaloi@gmail.com>
2022-11-26 05:02:52 +0100

    Updates

--- assets/BaseBtn.75665ed2.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index.d30916b8.js";import{o as r}from"./vuetify.dec8593d.js";import{O as e,P as a,Q as n,R as s}from"./vue.bed1aeba.js";const c={};function _(t,i){return e(),a(r,{border:"",color:"primary-button",class:"text-capitalize px-2 font-weight-regular"},{default:n(()=>[s(t.$slots,"default")]),_:3})}const m=o(c,[["render",_]]);export{m as _};
---
+++ assets/BaseBtn.a25b3ec7.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index.659721f2.js";import{o as r}from"./vuetify.dec8593d.js";import{O as e,P as a,Q as n,R as s}from"./vue.bed1aeba.js";const c={};function _(t,i){return e(),a(r,{border:"",color:"primary-button",class:"text-capitalize px-2 font-weight-regular"},{default:n(()=>[s(t.$slots,"default")]),_:3})}const m=o(c,[["render",_]]);export{m as _};
