fb6a8aff08baca6cb4b8feae28ab07240b4a7f5d
Santiago Aloi <santiagoaloi@gmail.com>
2022-11-26 05:31:58 +0100

    testing secure list drawer routes

---
+++ src/pages/@secure/account/OverviewTwo.vue
@@ -0,0 +1,19 @@
+<template>
+  <VList>
+    <VListItem title="Account Balance" subtitle="$ 0"> </VListItem>
+  </VList>
+
+  <VDivider />
+  <VContainer fluid>
+    <VAlert
+      title="Accounts"
+      icon="mdi-bank"
+      border="start"
+      border-color="deep-purple accent-4"
+      elevation="2"
+      class="mt-n2"
+    >
+      Hello from overview 2
+    </VAlert>
+  </VContainer>
+</template>
