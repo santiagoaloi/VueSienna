0089f2b852886f82b3958425ff09b37efa5d2b65
santiagoaloi <santiagoaloi@gmail.com>
2022-11-29 00:01:48 +0100

    css shortcuts

--- src/pages/homepage/components/HomeLanding.vue
+++ src/pages/homepage/components/HomeLanding.vue
@@ -2,7 +2,7 @@
   <SplitView col="6" src="x2.svg">
     <slot />
     <template #image-content>
-      <div class="d-flex align-center fill-height justify-center">
+      <div class="home-landing-img-wrap">
         <img style="width: 50vw" src="/bgCode.png" />
       </div>
     </template>
