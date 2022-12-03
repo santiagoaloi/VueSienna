d108338c5a35208b2087a2097886e8c246308add
santiagoaloi <santiagoaloi@gmail.com>
2022-12-03 01:11:28 +0100

    feat: OverLayOpacity playground

---
+++ src/pages/playground/projects/overlay-opacity/OverlayOpacity.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="text-center fill-height d-flex align-center justify-center">
+    <v-btn color="error" @click="overlay = !overlay"> Show Overlay </v-btn>
+
+    <v-overlay v-model="overlay"></v-overlay>
+  </div>
+</template>
+
+<script>
+getGridElementsForDisplay: (_, getters, rootState) => {
+  let thisDisplay = rootState.DisplayStore.thisDisplay
+
+  return thisDisplay
+    ? getters.getEnabledGridElements.filter(e =>
+        e.displays.includes(thisDisplay._id)
+      )
+    : []
+}
+
+export default {
+  data: () => ({
+    overlay: false,
+    opacity: '50%',
+  }),
+}
+</script>
+
+<style>
+.v-overlay > .v-overlay__scrim {
+  opacity: v-bind(opacity);
+}
+</style>
+
+<route lang="yaml">
+meta:
+  title: Overlay - Opacity
+  description: Playing  with opacity
+  icon:
+</route>
