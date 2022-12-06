5379954f92642953e612b6e2a67b4753692b16b7
santiagoaloi <santiagoaloi@gmail.com>
2022-12-06 15:19:33 +0100

    chore: componentized GApppBar

---
+++ src/@core/components/global-app-bar/GAppBarTab.vue
@@ -0,0 +1,26 @@
+<template>
+  <VTab
+    :ripple="false"
+    v-for="tab in tabs"
+    :key="tab.name"
+    :value="'tab-' + tab.name"
+    :to="tab.to"
+  >
+    {{ tab.name }}
+  </VTab>
+</template>
+<script setup>
+const store = useAuthStore()
+
+const tabs = computed(() => {
+  const loginName = store.isLoggedIn ? 'Profile' : 'Login'
+
+  const tabs = [
+    { name: 'Skriptag', to: '/', disabled: true },
+    { name: 'playground', to: '/playground', disabled: false },
+    { name: loginName, to: '/login', disabled: false },
+  ]
+
+  return tabs
+})
+</script>
