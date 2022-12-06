11fa7407c40cd25199bb07872b3a3b8801c67ae0
santiagoaloi <santiagoaloi@gmail.com>
2022-12-06 15:17:58 +0100

    chore: @vitejs/plugin-basic-ssl

--- vite.config.inports.js
+++ vite.config.inports.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 import pages from 'vite-plugin-pages'
 import viteFonts from 'vite-plugin-fonts'
 import vuetify from 'vite-plugin-vuetify'
+
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
 import layouts from 'vite-plugin-vue-layouts'
 import vueMacros from 'unplugin-vue-macros/vite'
 import autoImport from 'unplugin-auto-import/vite'
@@ -13,6 +16,7 @@ import removeConsole from 'vite-plugin-remove-console'
 import { default as injectMetadata } from './src/utils/autoInjectRouteMeta'
 
 export {
+  basicSsl,
   resolve,
   vue,
   pages,
