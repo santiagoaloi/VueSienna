3bf6c909ae0975c94824b6b3048221ba56639f12
santiagoaloi <santiagoaloi@gmail.com>
2022-12-06 15:24:50 +0100

    chore: v-show="auth.profile"

--- src/layouts/secure.vue
+++ src/layouts/secure.vue
@@ -1,6 +1,10 @@
 <template>
-  <section id="SecureLayout">
+  <section id="SecureLayout" v-show="auth.profile">
     <SDrawer />
     <SAppView />
   </section>
 </template>
+
+<script setup>
+const auth = useAuthStore()
+</script>
