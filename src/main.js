e142dac1e21e0bb8902feec32cbf7f3508102c43
santiagoaloi <santiagoaloi@gmail.com>
2022-12-08 06:07:58 +0100

    chore: copyright/url comments

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { Vue } from '@U/instanciateVue'
 
+/*
+ =========================================================
+ * VueSienna - v1.0.2
+ =========================================================
+
+ * Product Page: https://github.com/santiagoaloi/VueSienna
+ * Copyright 2022 to Present - Skriptag  (http://skriptag.com)
+
+ =========================================================
+ */
+
 // Init Vue and set new user on auth changes.
 auth.onAuthStateChanged(async user => {
   Vue(user)
