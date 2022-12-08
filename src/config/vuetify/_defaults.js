1a613ab424e925ab6c44b83a64c30e04a0aa7a59
santiagoaloi <santiagoaloi@gmail.com>
2022-12-08 06:05:10 +0100

    style: linting/formatting / defaults tunning

--- src/config/vuetify/_defaults.js
+++ src/config/vuetify/_defaults.js
@@ -10,9 +10,16 @@ export default {
   },
   VTextField: {
     variant: 'solo',
-    hideDetails: true,
+    hideDetails: 'auto',
     density: 'comfortable',
     bgColor: 'text-field-background',
+  },
+  VTextarea: {
+    variant: 'solo',
+    hideDetails: 'auto',
+    density: 'comfortable',
+    bgColor: 'text-field-background',
+    rows: 3,
   },
   VAutocomplete: {
     menuProps: { contentClass: 'autocomplete-list-bg-color', maxHeight: '200' },
@@ -23,5 +30,10 @@ export default {
     color: 'navigation-drawer',
     width: 280,
     permanent: true,
+  },
+  VAlert: {
+    border: 'start',
+    borderColor: 'purple-accent-4',
+    elevation: true,
   },
 }
