01b3346fa18657a9b125bf884e1904f6d26d2ffb
Santiago Aloi <santiagoaloi@gmail.com>
2022-12-10 16:03:54 +0100

    chore: vuetfiy aliases

--- src/config/vuetify/index.js
+++ src/config/vuetify/index.js
@@ -1,5 +1,6 @@
 import themes from './_themes'
+import aliases from './_aliases'
 import * as icons from './_icons'
 import defaults from './_defaults'
 
-export { themes, icons, defaults }
+export { themes, icons, defaults, aliases }
