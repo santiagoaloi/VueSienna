47acab1d69fc03a4e1e581fba350b33350285ff4
Santiago Aloi <santiagoaloi@gmail.com>
2022-12-10 16:05:06 +0100

    + ThemeSwitch alias use

--- src/@core/components/global-app-bar/GAppBarThemeSwitch.vue
+++ src/@core/components/global-app-bar/GAppBarThemeSwitch.vue
@@ -1,16 +1,8 @@
 <template>
   <div class="g-appbar-switch-wrapper">
-    <VSwitch
+    <ThemeSwitch
       title="Switch theme"
-      true-icon="$mdiMoonWaxingCrescent"
-      false-icon="$mdiWhiteBalanceSunny"
-      inset
       class="d-flex text-white"
-      hide-details
-      :ripple="false"
-      color="#6B6975"
-      true-value="dark"
-      false-value="light"
       v-model="currentTheme"
     />
   </div>
