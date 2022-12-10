feaed330399c12b97b6344d3f6e0a031601f6e68
Santiago Aloi <santiagoaloi@gmail.com>
2022-12-10 16:00:32 +0100

    +++ comments

--- src/utils/autoImportModules.js
+++ src/utils/autoImportModules.js
@@ -1,2 +1,7 @@
+/*
+ * Anything under @/modules
+ * will be auto-imported.
+ */
+
 const modules = import.meta.globEager('@M/*.js')
 export default app => Object.values(modules).map(m => m.install(app))
