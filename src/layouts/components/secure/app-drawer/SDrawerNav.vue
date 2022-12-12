bb334ac740abae2a2fd6ce7b11cd6e0baf2fec65
Santiago Aloi <santiagoaloi@gmail.com>
2022-12-12 18:42:29 +0100

    refactor: removed /account/overview route

--- src/layouts/components/secure/app-drawer/SDrawerNav.vue
+++ src/layouts/components/secure/app-drawer/SDrawerNav.vue
@@ -3,12 +3,6 @@
     item-props
     :items="[
       {
-        title: 'Overview',
-        value: 'overview',
-        to: '/account/overview',
-        prependIcon: '$mdiViewDashboardOutline',
-      },
-      {
         title: 'Public Profile',
         value: 'PublicProfile',
         to: '/account/publicprofile',
