8d1311a72b7cac2706b97469cd9d74bd4cc25851
Santiago Aloi <santiagoaloi@gmail.com>
2022-12-12 18:38:47 +0100

    chore: comments

--- src/App.vue
+++ src/App.vue
@@ -17,5 +17,7 @@
 <script setup>
 // Pinia appStore module is persisted.
 // currentTheme values are light/dark
+
+// toRefs is needed so reactivity continues in destructure.
 const { currentTheme } = toRefs(useAppStore())
 </script>
