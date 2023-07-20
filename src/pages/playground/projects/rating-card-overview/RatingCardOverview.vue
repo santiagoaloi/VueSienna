095451e9b08eb8ad179da1c7273677857ca6199e
santiagoaloi <santiagoaloi@gmail.com>
2023-07-20 18:27:08 +0200

    cumulative

--- src/pages/playground/projects/rating-card-overview/RatingCardOverview.vue
+++ src/pages/playground/projects/rating-card-overview/RatingCardOverview.vue
@@ -1,66 +1,67 @@
 <template>
-  <VContainer class="d-flex align-center justify-center flex-wrap fill-height">
-    <VCard
-      elevation="13"
-      class="d-flex flex-column mx-auto py-8 rounded-lg"
-      height="500"
-      width="360"
-      color="rgba(50, 58, 68, 0.5)"
+    <VContainer class="d-flex align-center justify-center flex-wrap fill-height">
+
+  <v-card
+    class="d-flex flex-column mx-auto py-8"
+    elevation="10"
+    height="500"
+    width="360"
+  >
+    <div class="d-flex justify-center mt-auto text-h5 ">
+      Rating overview
+    </div>
+
+    <div class="d-flex align-center flex-column my-auto">
+      <div class="text-h2 mt-5">
+        3.5
+        <span class="text-h6 ml-n3">/5</span>
+      </div>
+
+      <v-rating
+        :model-value="3.5"
+        color="yellow-darken-3"
+        half-increments
+      ></v-rating>
+      <div class="px-3">3,360 ratings</div>
+    </div>
+
+    <v-list
+      bg-color="transparent"
+      class="d-flex flex-column-reverse"
+      density="compact"
     >
-      <div class="d-flex justify-center mt-auto text-h5 text-capitalize">
-        Rating overview
-      </div>
+      <v-list-item v-for="(rating,i) in 5" :key="i">
+        <v-progress-linear
+          :model-value="rating * 15"
+          class="mx-n5"
+          color="yellow-darken-3"
+          height="20"
+          rounded
+        ></v-progress-linear>
 
-      <div class="d-flex align-center flex-column my-auto">
-        <div class="text-capitalize">
-          <div class="text-h2 mt-5">
-            4.1
-            <span class="text-h6 ml-n3">/5</span>
+        <template v-slot:prepend>
+          <span>{{ rating }}</span>
+          <v-icon icon="mdi-star" class="mx-3"></v-icon>
+        </template>
+
+        <template v-slot:append>
+          <div class="rating-values">
+            <span class="d-flex justify-end"> {{ rating * 224 }} </span>
           </div>
-        </div>
-
-        <div>
-          <VRating
-            :model-value="4.5"
-            half-increments
-            readonly
-            color="yellow-darken-3"
-          />
-        </div>
-        <div class="px-3">2,250 ratings</div>
-      </div>
-
-      <VList
-        class="d-flex flex-column-reverse"
-        bg-color="transparent"
-        density="compact"
-      >
-        <VListItem v-for="raiting in 5">
-          <v-progress-linear
-            height="20"
-            :model-value="raiting * 18 + 8"
-            color="yellow-darken-3"
-            rounded
-          />
-          <template v-slot:prepend>
-            <div style="width: 10px">
-              <span>{{ raiting + 1 }}</span>
-            </div>
-            <div class="d-flex">
-              <VIcon class="mx-3">$mdiStar</VIcon>
-            </div>
-          </template>
-
-          <template v-slot:append>
-            <div style="width: 50px">
-              <span class="ml-3"> {{ raiting * 224 + 2 }} </span>
-            </div>
-          </template>
-        </VListItem>
-      </VList>
-    </VCard>
-  </VContainer>
+        </template>
+      </v-list-item>
+    </v-list>
+  </v-card>
+    </VContainer>
 </template>
+
+<style>
+
+.rating-values {
+   width: 25px;
+}
+</style>
+
 <route lang="yaml">
 meta:
   title: Rating component 1 (card)
